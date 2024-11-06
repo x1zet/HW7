4fbf455528e3b3311305fe750cc3d32abcee848f
x1zet <155456277+x1zet@users.noreply.github.com>
2024-11-06 23:25:11 +0300

    Ready

--- Task 2/src/Main.java
+++ Task 2/src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         //todo Тут увольняем айтишников
+        int itWorkers = 100;
+
+        for (int i = 1; i < itWorkers; i++){
+            if (i % 2 == 0){
+                System.out.println("Уволен работник с id: " + i);
+            }
+        }
     }
 }
